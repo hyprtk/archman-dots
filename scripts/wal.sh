27ad5a5ff99fffe33731258bfbba380e70f249df
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-05-06 10:05:01 +0100

    initial upload

---
+++ scripts/wal.sh
@@ -0,0 +1,11 @@
+#
+# by hyprtk (Kori Tk) (2026)
+# ----------------------------------------------------- 
+
+wal -q -i ~/Pictures/Wallpapers/ 
+
+killall waybar 2>/dev/null; ~/archman-dots/waybar/launch.sh
+
+~/archman-dots/papirus-icons/scripts/change-icons.sh
+
+exit
